d10a2336de8250757e18016e26bac828697d4a74
Sruti Modekurty <msrutiv@gmail.com>
2019-02-07 17:07:03 -0500

    Add backend to get and store data in dynamoDB

---
+++ backend-aws/handler.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('data-table')
+
+def format_response(status_code, body):
+    response = {
+        "statusCode": status_code,
+        "body": json.dumps(body)
+    }
+    return response
+
+def get_data(event, context):   
+    response = table.get_item(Key={"dataId": "1"})
+    print(response)
+    data = response.get("Item")
+    print(data)
+    return format_response(200, data)
+
+def store_data(event, context):
+    print(event)  
+    item = json.loads(event.get('body'))
+    item["dataId"] = "2"
+    response = table.put_item(Item=item)
+    return format_response(200, "Stored the data")
